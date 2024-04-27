fd64fec90a89ee3395eedd401858be95e7e149cd
Monica Palko <m.a.palko07@gmail.com>
2024-04-27 14:43:59 -0400

    refactored get all reservations code to use database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -77,8 +77,26 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+      FROM reservations
+      JOIN properties ON properties.id = reservations.property_id
+      LEFT JOIN property_reviews ON property_reviews.reservation_id = reservations.id
+      WHERE reservations.guest_id = $1
+      GROUP BY reservations.id, properties.id
+      ORDER BY reservations.start_date
+      LIMIT $2`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.error("query error", err.message);
+    });
 };
+
 
 /// Properties
 
